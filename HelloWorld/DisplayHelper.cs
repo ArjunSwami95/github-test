220336e5d1f4ec2abe4289b3a51c6aa8b4fc35d5
Arjun Swami <arjunswami@microsoft.com>
2022-12-21 05:36:58 -0800

    My First Commit

---
+++ HelloWorld/DisplayHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    public static class DisplayHelper
+    {
+        public static void DisplayText(string s)
+        {
+            Console.WriteLine($"this is going to display the text :{s}");
+        }
+    }
+}
