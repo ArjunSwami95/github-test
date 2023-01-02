a397962fdcc972cccbe4bcd03f0a562121dff218
Arjun Swami <arjunswami@microsoft.com>
2023-01-02 07:41:49 -0800

    Hanuman

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            DisplayHelper.DisplayText("Jai Sri Ram!!");
+            DisplayHelper.DisplayText("Jai Sri Ram!! Jai Hanuman");
         }
     }
 }
